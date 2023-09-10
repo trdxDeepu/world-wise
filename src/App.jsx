921358110398ec18a371d5cf68f10ce18a4165b9
trdxlucifer <singhdepu566@gmail.com>
2023-09-10 21:27:46 +0530

    added more component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
+// import AppLayout from "./pages/AppLayout";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout/>} />
+        {/* <Route path="app" element={<AppLayout/>} /> */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-const PageNotFound = () => {
+export default function PageNotFound() {
   return (
     <div>
-      <h1>Page Not Found ); </h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
-};
-
-export default PageNotFound;
+}
