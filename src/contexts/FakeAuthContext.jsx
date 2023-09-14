17303eff0517de25ac17d571ef0696b6573dd1fd
trdxlucifer <singhdepu566@gmail.com>
2023-09-14 15:45:34 +0530

    added login and logout functionality

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,17 +1,58 @@
-/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line react-refresh/only-export-components
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-function AuthProvider({children}){
-    return <AuthContext.Provider>{children}</AuthContext.Provider>
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
-function useAuth(){
-    const context = useContext()
-    if(context === undefined) throw new Error("AuthContext was used outside provider")
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside AuthProvider");
+  return context;
+}
+
+export { AuthProvider, useAuth };
